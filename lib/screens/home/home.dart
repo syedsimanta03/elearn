9488c63174ef00c3eadba35b3d85d46c5dc77097
syedsimanta03 <syed.simanta06@gmail.com>
2022-12-03 15:09:10 +0600

    eleaning

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Home Text'),
+      ),
+    );
+  }
+}
